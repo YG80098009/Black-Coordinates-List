6e1c8c9ea581e8a3955f05a205a37578f3abae3c
Ariel halifa <arielhalifa@gmail.com>
2026-01-14 10:01:27 +0200

    sending for code review

---
+++ service-a/app/services.py
@@ -0,0 +1,17 @@
+import requests
+
+class Routs:
+    
+    @staticmethod
+    def cleaning_data(dict_json):
+        dict = {"query": dict_json["query"],
+            "lat": dict_json["lat"],
+            "lon": dict_json["lon"]}
+        return dict
+    
+
+
+    @staticmethod
+    def send_to_server_b(clean_data):
+        x = requests.post(f"http://localhost:8001/store/{clean_data}")
+        return x
